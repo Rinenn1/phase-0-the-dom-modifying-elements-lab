97735de3ee506a34b861a58435e47aff58805640
Isaac <bennisaac24@gmail.com>
2024-12-18 12:57:33 +0300

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+//removing the main tag with an id of 'main'
+const main = document.getElementById('main');
+
+main.remove();
+
+//has a 'newHeader' variable pointing to an <h1> node and setting it's id name to equal 'victory'
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+//the <h1> includes the text \"is the champion\" inside it"
+newHeader.textContent = "ISAAC is the champion";
